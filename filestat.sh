9640af5c6cd271af50fafbc678d5872ce5cd517b
soyccan <soytwww@gmail.com>
2018-10-11 19:30:35 +0800

    update filestat.sh to run on FreeBSD

--- filestat.sh
+++ filestat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#find  -type f | xargs du -bs | awk '{print NR":", $1"bytes", $2}' 
-ls --almost-all --recursive --block-size 1 -l | sed -n '/^[-d]/p' | sort --key 5 --reverse --numeric-sort | awk 'BEGIN{fc=dc=sz=0} /^-/{fc+=1; sz+=$5; if (fc <= 5) print fc ": " $5 "bytes " $9} /^d/{dc+=1} END{print "File Count: " fc "\nDir Count: " dc "\nTotal Size: " sz "bytes"}'
+#find  -type f | xargs du -bs | awk '{print NR":", $1"bytes", $2}'
+ls -ARl | sed -n '/^[-d]/p' | sort --key 5 --reverse --numeric-sort | awk 'BEGIN{fc=dc=sz=0} /^-/{fc+=1; sz+=$5; if (fc <= 5) print fc ": " $5 "bytes " $9} /^d/{dc+=1} END{print "File Count: " fc "\nDir Count: " dc "\nTotal Size: " sz "bytes"}'
